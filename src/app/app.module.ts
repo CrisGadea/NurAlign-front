3e68724b1545c4a5e23da4bd9e39a57835005e95
CrisGadea <cristianhernangadea@gmail.com>
2024-05-06 15:23:23 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private service: LoginService) { }
+
+  login(){
+    let data = {
+      email: 'admin',
+      password: 'admin'
+    }
+    this.service.login(data).subscribe((response)=>{
+      console.log(response);
+      alert(response === true ? 'Login Success' : 'Login Failed');
+    },(error)=>{
+      console.log(error);
+    });
+  }
+
+
+  }
