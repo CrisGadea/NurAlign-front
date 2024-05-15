cb6935089e8433db2d080764c4385f16365f140d
axelivanabalos3451 <axelabalosss@gmail.com>
2024-05-15 10:16:28 -0300

    generate new component calendar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
+=======
+import { TherapistCalendarComponent } from './therapist-calendar/therapist-calendar.component';
+>>>>>>> b6a9a23 (generate new component calendar)
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  {path:'Calendar',component:TherapistCalendarComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent},
   {path:'dashboard', component:DashboardComponent},
