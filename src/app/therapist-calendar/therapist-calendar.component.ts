df829b7b34f16603d97ecabb6cd9c3ee713516bc
axelivanabalos3451 <axelabalosss@gmail.com>
2024-05-17 20:36:51 -0300

    falta header y footer

--- src/app/therapist-calendar/therapist-calendar.component.ts
+++ src/app/therapist-calendar/therapist-calendar.component.ts
@@ -38,18 +38,27 @@ export class TherapistCalendarComponent implements OnInit {
 
   ngOnInit(): void {
     const currentDate = moment();
+    //se le suma uno para que el rango sea de 1 a 12
     this.getDaysFromDate(currentDate.month() + 1, currentDate.year());
   }
 
   getDaysFromDate(month: number, year: number): void {
+  //obtiene el primer dia del mes
     const startDate = moment(`${year}-${month}-01`);
+    //obtiene el ultimo dia del mes
     const endDate = startDate.clone().endOf('month');
     this.dateSelect = startDate;
 
+
+    //te da los dias que exite entre la fecha de inicio y la fecha final del mes
     const numberDays = endDate.date();
 
+
+//se genera el array de los dias
     const arrayDays = Array.from({ length: numberDays }, (_, i) => {
+      //como los arrays empiezan en 0 se le suma uno
       const day = i + 1;
+      //permite saber que dia de la semana es. Basicamente guardas el dia
       const dayObject = moment(`${year}-${month}-${day}`);
       return {
         name: dayObject.format("dddd"),
@@ -57,7 +66,7 @@ export class TherapistCalendarComponent implements OnInit {
         indexWeek: dayObject.isoWeekday()
       };
     });
-
+//se le asigna a mount select la colceccion de dias el array de todos los dias
     this.monthSelect = arrayDays;
   }
 
