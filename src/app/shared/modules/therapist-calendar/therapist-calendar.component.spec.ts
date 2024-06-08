9d3edb55ee1ceab4c2b93375b68a54c6f2aa4a85
axelivanabalos3451 <axelabalosss@gmail.com>
2024-06-07 23:00:04 -0300

    se agrego un test

--- src/app/shared/modules/therapist-calendar/therapist-calendar.component.spec.ts
+++ src/app/shared/modules/therapist-calendar/therapist-calendar.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 
 import { TherapistCalendarComponent } from './therapist-calendar.component';
+import * as moment from 'moment';
 
 
 @Component({
@@ -33,5 +34,13 @@ describe('TherapistCalendarComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('do sessionByFetch',()=>{
+    const testDate = { value: 23 };//clickea este boton
+    const expectedDate = moment('2024-05-23');//fecha esperada
+    component.dateSelect = moment('2024-05');//se setea la fecha y el mes actual
+    component.clickDay(testDate);
 
+    expect(component.popupDate).toBe(expectedDate.format("DD/MM/YYYY"));
+    
+  })
 });
